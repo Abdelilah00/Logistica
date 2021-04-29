9b65ed038302ae2959a43d80c149701f67e380f6
abdelilah dehaoui <aboudahaoui123456789@gmail.com>
2021-04-29 03:56:19 +0000

    challenge accepted and still in solving

--- Logistica-BackOffice/src/app/pages/transactions/inputs/transaction-details-create/transaction-details-create.component.ts
+++ Logistica-BackOffice/src/app/pages/transactions/inputs/transaction-details-create/transaction-details-create.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {GridComponent} from '@progress/kendo-angular-grid';
+import {TransactionDetail} from '../../../../core/models/auth.models';
 
 @Component({
   selector: 'app-transaction-details-create',
@@ -8,59 +9,96 @@ import {GridComponent} from '@progress/kendo-angular-grid';
   styleUrls: ['./transaction-details-create.component.scss']
 })
 export class TransactionDetailsCreateComponent implements OnInit {
-  specifications = [];
-  // @Output() specificationsChange = new EventEmitter<any>();
-  specificationsForm: FormGroup;
-  @ViewChild('grid', {static: true}) grid: GridComponent;
+  products = Array<TransactionDetail>();
+  originalProducts = [];
+  productsForm: FormGroup;
+  isEditMode = true;
+  @ViewChild('grid') grid: GridComponent;
 
   constructor(private formBuilder: FormBuilder) {
   }
 
   // convenience getters for easy access to form fields
-
-  get formArray(): FormArray {
-    return this.specificationsForm.controls.formArray as FormArray;
+  get f() {
+    return this.productsForm.controls;
   }
 
-  ngOnInit(): void {
+  get fa() {
+    return this.f.formArray as FormArray;
+  }
+
+  ngOnInit() {
+
     // initialise products form with empty form array
-    this.specificationsForm = this.formBuilder.group({
+    this.productsForm = this.formBuilder.group({
       formArray: new FormArray([])
     });
   }
 
+  onEdit() {
+    // store copy of original products in case cancelled
+    this.originalProducts = [...this.products];
 
-  onAdd(): void {
+    // reset / initialise form fields
+    this.resetForm();
+
+    // set all rows to edit mode to display form fields
+    this.editAllRows();
+    this.isEditMode = true;
+  }
+
+  onAdd() {
     // add item to products array
-    this.closeAllRows();
-    this.specifications.push({});
+    this.products.push(new TransactionDetail());
 
     // add new form group to form array
     const formGroup = this.createFormGroup();
-    this.formArray.push(formGroup);
+    this.fa.push(formGroup);
 
     // set new row to edit mode in kendo grid
-    this.grid.editRow(this.specifications.length - 1, formGroup);
+    this.grid.editRow(this.products.length - 1, formGroup);
   }
 
-  onRemove(index): void {
+  onRemove(index) {
     // rows must all be closed while removing products
     this.closeAllRows();
 
     // remove product and product form group
-    this.specifications.splice(index, 1);
-    this.formArray.removeAt(index);
+    this.products.splice(index, 1);
+    this.fa.removeAt(index);
 
     // reset all rows back to edit mode
-    // this.editAllRows();
-    this.onGridChange();
+    this.editAllRows();
   }
 
-  public onGridChange(): void {
-    // this.closeAllRows();
-    this.specifications = this.formArray.value;
-    // this.specificationsChange.emit(this.specifications);
+  onSave() {
+    // mark all fields as touched to highlight any invalid fields
+    this.productsForm.markAllAsTouched();
+
+    // stop here if form is invalid
+    if (this.productsForm.invalid) {
+      alert('FORM INVALID :(');
+      return;
+    }
+
+    // copy form data to products array on success
+    this.products = this.fa.value;
+
+    console.log(this.products);
+    this.closeAllRows();
+    this.isEditMode = false;
   }
+
+  onCancel() {
+    this.closeAllRows();
+
+    // reset products back to original data (before edit was clicked)
+    this.products = this.originalProducts;
+
+    this.isEditMode = false;
+  }
+
+  // helper methods
 
   public createFormGroup(dataItem: any = {}): FormGroup {
     return this.formBuilder.group({
@@ -73,19 +111,28 @@ export class TransactionDetailsCreateComponent implements OnInit {
     });
   }
 
-  private editAllRows(): void {
+  private editAllRows() {
     // set all rows to edit mode to display form fields
-    this.specifications.forEach((x, i) => {
-      this.grid.editRow(i, this.formArray.controls[i].value);
+    this.products.forEach((x, i) => {
+      // @ts-ignore
+      this.grid.editRow(i, this.fa.controls[i]);
     });
-
   }
 
-  private closeAllRows(): void {
+  private closeAllRows() {
     // close all rows to display readonly view of data
-    this.specifications.forEach((x, i) => {
+    this.products.forEach((x, i) => {
       this.grid.closeRow(i);
     });
   }
+
+  private resetForm() {
+    // clear form array and create a new form group for each product
+    this.fa.clear();
+    this.products.forEach((x, i) => {
+      this.fa.push(this.createFormGroup(x));
+    });
+  }
+
 
 }
