132a73fceb87feff1de32d1c275c3ba52064c8b7
abdelilah dehaoui <aboudahaoui123456789@gmail.com>
2021-04-30 02:46:41 +0000

    start creating modules

--- Logistica-BackOffice/src/app/pages/products/products.module.ts
+++ Logistica-BackOffice/src/app/pages/products/products.module.ts
@@ -4,19 +4,18 @@ import {ProductListComponent} from './product-list/product-list.component';
 import {RouterModule, Routes} from '@angular/router';
 import {SupplierCreateComponent} from '../suppliers/supplier-create/supplier-create.component';
 import {ProductCreateComponent} from './product-create/product-create.component';
-import {ProductEditeComponent} from './product-edite/product-edite.component';
 import {ProductEditComponent} from './product-edit/product-edit.component';
 
 
 const routes: Routes = [
   {path: 'list', component: ProductListComponent},
-  {path: 'edit/:id', component:},
+  {path: 'edit/:id', component: ProductEditComponent},
   {path: 'create', component: SupplierCreateComponent},
   {path: '**', redirectTo: 'list'},
 ];
 
 @NgModule({
-  declarations: [ProductListComponent, ProductCreateComponent, ProductEditeComponent, ProductEditComponent],
+  declarations: [ProductListComponent, ProductCreateComponent, ProductEditComponent],
   imports: [RouterModule.forChild(routes),
 
     CommonModule
