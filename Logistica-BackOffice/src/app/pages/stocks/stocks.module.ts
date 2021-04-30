7bfe76a6a1ad857c01842a6ce438b83c6c77db7d
abdelilah dehaoui <aboudahaoui123456789@gmail.com>
2021-04-30 03:06:14 +0000

    start creating modules

--- Logistica-BackOffice/src/app/pages/stocks/stocks.module.ts
+++ Logistica-BackOffice/src/app/pages/stocks/stocks.module.ts
@@ -3,14 +3,12 @@ import {CommonModule} from '@angular/common';
 import {StockListComponent} from './stock-list/stock-list.component';
 import {StockCreateComponent} from './stock-create/stock-create.component';
 import {RouterModule, Routes} from '@angular/router';
-import {SupplierListComponent} from '../suppliers/supplier-list/supplier-list.component';
-import {SupplierCreateComponent} from '../suppliers/supplier-create/supplier-create.component';
 
 
 const routes: Routes = [
-  {path: 'list', component: SupplierListComponent},
-  {path: 'edit/:id', component: SupplierCreateComponent},
-  {path: 'create', component: SupplierCreateComponent},
+  {path: 'list', component: StockListComponent},
+  {path: 'edit/:id', component: StockCreateComponent},
+  {path: 'create', component: StockCreateComponent},
   {path: '**', redirectTo: 'list'},
 ];
 
